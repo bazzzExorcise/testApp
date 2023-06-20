46a9a86b8f2ac7768c876cc1a26b96782f10df26
alif mahendra <nacubes20@gmail.com>
2023-06-20 11:47:22 +0700

    update bersama naj

--- Server/index.php
+++ Server/index.php
@@ -16,8 +16,8 @@ if($row['status'] == "admin") {
   <div class="relative w-full flex flex-col items-center max-w-sm max-h-screen overflow-auto">
     <div class="w-full px-4 flex flex-col pt-2 gap-4">
       <div>
-        <h1 class="-mb-2 font-extrabold text-3xl">Ujian Hari ini</h1>
-        <p class="text-sm">19 januari 2023</p>
+        <h1 class="-mb-2 font-extrabold text-3xl">Masuk ruangan ujian</h1>
+        <p class="text-sm date-format"></p>
       </div>
       <div class="content flex flex-col gap-2">
         <div class="w-full text-white p-4 group rounded-lg border-b-4 border-2 border-black bg-orange-500">
@@ -29,37 +29,41 @@ if($row['status'] == "admin") {
           <p class="text-sm flex gap-1 items-center group">10:00 WIB</p>
         </div>
       </div>
-      <div>
-        <h1 class="mt-4 -mb-2 font-extrabold text-3xl">Ujian Besok</h1>
-        <p class="text-sm">20 januari 2023</p>
-      </div>
-      <div class="content flex flex-col gap-2">
-        <div class="w-full text-white p-4 group rounded-lg border-b-4 border-2 border-black bg-orange-500">
-          <h1 class="font-extrabold text-3xl">Kimia</h1>
-          <p class="text-sm flex gap-1 items-center group">20 januari 2023 - 07.30 WIB</p>
-        </div>
-        <div class="w-full p-4 group rounded-lg border-b-4 border-2 border-black bg-sky-400">
-          <h1 class="font-extrabold text-3xl">Alquran Hadist</h1>
-          <p class="text-sm flex gap-1 items-center group">20 januari 2023 - 10:00 WIB</p>
-        </div>
-        <div class="w-full flex justify-end">
-          <a href="system/logout.php" class="font-extrabold rounded-lg border-b-4 border-2 border-black bg-red-500 px-4 py-2">log out</a>
-        </div>
-      </div>
     </div>
   </div>
-  <div class="font-extrabold block-wall opacity-0 -z-30 fixed bg-white/50 backdrop-blur-sm duration-500 top-0 left-0 h-screen w-full grid place-items-center">
+  <div class="font-extrabold room-wall opacity-0 -z-30 fixed bg-white/50 backdrop-blur-sm duration-500 top-0 left-0 h-screen w-full grid place-items-center">
     <div class="p-4 max-w-xs bg-sky-300 w-full rounded-md flex flex-col items-center justify-center gap-2 border-2 border-black">
-      <h1 class="text-2xl text-center">Mohon Maaf</h1>
-      <p class="text-sm text-center">anda belum melakukan login dalam situs ini seilahkan login dan lanjutkan</p>
+      <h1 class="text-2xl text-center">masuk ruangan</h1>
+      <p class="text-sm text-center">silahkan pilih ruangan yang ingin anda jaga</p>
+      <select name="" id="select">
+        <option value=""></option>
+      </select>
       <a href="login.php" class="text-white bg-black rounded-lg w-full py-2 text-center">Login</a>
     </div>
   </div>
+  <a href="system/logout.php">log out</a>
   <script>
-    function blockWall() {
-      $(".block-wall").removeClass("-z-30", "opacity-0");
-      $(".block-wall").addClass("z-30", "opacity-100");
-    }
+    $(document).ready(function () {
+      var date = new Date();
+
+      var monthNames = [
+        "januari", "februari", "maart", "april", "mei", "juni",
+        "juli", "augustus", "september", "oktober", "november", "december"
+      ];
+
+      var day = date.getDate();
+      var month = date.getMonth();
+      var year = date.getFullYear();
+
+      var formattedDate = day + " " + monthNames[month] + " " + year;
+      $(".date-format").html(formattedDate);
+
+      function getRoom() {
+        $.get("../system/ruangan-select.php", function(data) {
+          $("#select").html(data);
+        })
+      }
+    });
   </script>
 </body>
 </html>
--- Server/keeper-section/index.php
+++ Server/keeper-section/index.php
@@ -6,7 +6,7 @@
         <h1 class="-mb-2 font-extrabold text-3xl">Ruangan Ujian</h1>
       </div>
       <div class="content flex flex-col gap-2">
-        <div class="w-full group pt-4 rounded-lg border-b-4 border-2 border-black bg-cyan-400">
+        <div class="w-full button-room cursor-pointer group pt-4 rounded-lg border-b-4 border-2 border-black bg-cyan-400">
           <h1 class="font-extrabold px-4 text-3xl">Masuk Ruangan</h1>
           <p class="text-sm flex gap-1 px-4 pb-4 items-center group">masukan ruangan yang anda tuju<ion-icon class="duration-300 group-hover:translate-x-2" name="arrow-round-forward"></ion-icon></p>
           <button class="bg-black w-full py-2 text-white text-center">masuk kedalam ruangan ujian</button>
@@ -25,6 +25,7 @@
           </div>
         </div>
         <button class="w-full bg-yellow-500 border-2 soal-button border-black border-b-4 py-2 rounded-lg">tambah soal</button>
+        <a href="../system/logout.php" class="bg-black">logout</a>
       </div>
     </div>
   </div>
@@ -37,17 +38,51 @@
         <input type="text" name="name" class="start border-2 border-black input-update rounded-lg py-2 px-3 placeholder:text-black" placeholder="nama ruangan" id="">
         <input type="text" name="kelas" class="start border-2 border-black input-update rounded-lg py-2 px-3 placeholder:text-black" placeholder="kelas" id="">
         <button type="submit" class="bg-green-400 rounded-lg w-full py-2 text-center border-2 border-black">Buat</button>
-        <a href="../system/logout.php" class="bg-black">logout</a>
+      </form>
+    </div>
+  </div>
+  <div class="font-extrabold enter-room opacity-0 -z-30 fixed bg-white/50 backdrop-blur-sm duration-500 top-0 left-0 h-screen w-full grid place-items-center">
+    <div class="p-4 max-w-xs bg-sky-300 w-full rounded-md flex flex-col items-center justify-center gap-2 border-2 border-black">
+      <div class="w-full flex justify-end soal-button"><ion-icon name="close"></ion-icon></div>
+      <h1 class="text-2xl text-center">Masuk Ruangan</h1>
+      <form class="enter w-full flex flex-col gap-2" action="../system/enter-room.php" method="post">
+        <div class="w-full update-box"></div>
+        <select name="name" class="start border-2 border-black input-update rounded-lg py-2 px-3 placeholder:text-black" placeholder="nama ruangan" id="select">
+          
+        </select>
+        <button type="submit" class="bg-green-400 rounded-lg w-full py-2 text-center border-2 border-black">Masuk ruangan</button>
       </form>
     </div>
   </div>
 
   <script>
     $(document).ready(function () {
-      $(".soal-button").click(function (e) { 
+      getRoom()
+      $(".enter").submit(function (e) { 
         e.preventDefault();
-        $(".add-soal").toggleClass("opacity-0 -z-30 z-30");
+        $.ajax({
+          type: $(this).attr("method"),
+          url: $(this).attr("action"),
+          data: $(this).serialize(),
+          success: function (response) {
+            console.log(response);
+          }
+        });
       });
+    });
+
+    $(".soal-button").click(function (e) { 
+      e.preventDefault();
+      $(".add-soal").toggleClass("opacity-0 -z-30 z-30");
+    });
+    function getRoom() {
+      $.get("../system/ruangan-select.php", function(data) {
+        $("#select").html(data);
+      })
+    }
+    $(".button-room").click(function (e) { 
+      e.preventDefault();
+      $(".enter-room").toggleClass("opacity-0 -z-30 z-30");
     });
   </script>
 </body>
